85be8caa5cecfe742ce08001cceca44aa812f93e
ThuanyMendonca <ThuanyMendonca>
2018-07-22 11:58:50 -0300

    Funcoes

--- Funções/cidadaoPrimeiraClasse.js
+++ Funções/cidadaoPrimeiraClasse.js
@@ -1,2 +1,37 @@
 // Função em JS é First-Class Object (Citizens)
 // Higher-order function
+
+// Criar de forma literal
+function fun1(){ }
+
+// Armazenar em uma variável
+const fun2 = function () { }
+
+const array = [function (a, b){ return a + b }, fun1, fun2]
+console.log(array[0](2, 3))
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () { return 'Opa' }
+console.log(obj.falar())
+
+// Passar função como param
+function run(fun){
+    fun()
+}
+
+// Passando uma função por parametro para a função run()
+run(function () { 
+    console.log('Executando...') 
+})
+
+function soma(a,b){
+    return function(c){
+        console.log(a + b + c)
+    }
+}
+
+soma(2, 3)(4)
+// Armazenar na variável o valor da soma da função
+const cincoMais = soma(2, 3)
+cincoMais(4)
---
+++ Funções/paramsPadrao.js
@@ -0,0 +1,9 @@
+// estrategia 1 para gerar valor padrão
+function soma1(a, b, c){
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    return a + b + c
+}
+
+console.log(soma1())
